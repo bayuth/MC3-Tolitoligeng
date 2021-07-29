a8e223385094f8c7c99d259bea8fc8288ecf165f
Achmad Fathullah <amedixro@gmail.com>
2021-07-29 10:34:17 +0700

    initial project

---
+++ Legalin/Legalin/LegalinApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegalinApp.swift
+//  Legalin
+//
+//  Created by Achmad Fathullah on 29/07/21.
+//
+
+import SwiftUI
+
+@main
+struct LegalinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
