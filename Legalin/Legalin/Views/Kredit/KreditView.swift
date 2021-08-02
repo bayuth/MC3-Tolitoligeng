ff13f39af66e347f32137c3ab9fd34470665da96
Achmad Fathullah <amedixro@gmail.com>
2021-08-02 09:02:28 +0700

    add asset empty kredit

---
+++ Legalin/Legalin/Views/Kredit/KreditView.swift
@@ -0,0 +1,20 @@
+//
+//  KreditView.swift
+//  Legalin
+//
+//  Created by Achmad Fathullah on 02/08/21.
+//
+
+import SwiftUI
+
+struct KreditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KreditView_Previews: PreviewProvider {
+    static var previews: some View {
+        KreditView()
+    }
+}
