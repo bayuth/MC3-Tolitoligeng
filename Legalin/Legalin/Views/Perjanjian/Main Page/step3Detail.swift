92925b40215a3449be96ab0afe09b0f04d98386b
Rahmannur Rizki Syahputra <srahmannur@gmail.com>
2021-08-09 15:21:54 +0700

    benerin color datepicker sama slider

--- Legalin/Legalin/Views/Perjanjian/Main Page/step3Detail.swift
+++ Legalin/Legalin/Views/Perjanjian/Main Page/step3Detail.swift
@@ -25,52 +25,60 @@ struct step3Detail: View {
     }()
     @State private var valueDateJatuhTempo = "Pilih tanggal"
     var body: some View {
-        ScrollView{
-            VStack(alignment: .leading){
-                pageIndicator(progressNumber: 3, progressName: "Detail Pinjaman", progressDetail: "Berikutnya: Agunan").padding(.bottom, 15).padding(.top,25)
-                FormView(title: "Tujuan Peminjaman", profileValue: $tujuanPeminjaman, keyboardNum: false)
-                SliderViewWithForm(text1: "Pinjaman Maksimal", text2: "Rp 50000000", title: "Jumlah Pinjaman", type: 0)
-                SliderViewWithForm(text1: "Bunga Maksimal", text2: "6 % per tahun", title: "Bunga", type: 1)
-                SliderViewWithForm(text1: "Tenor Maksimal", text2: "24 bulan", title: "Tenor", type: 2)
-                FormViewWithInfo(title: "Metode Pembayaran", profileValue: $title, showButton: true, showButtonInfo: false, info: "", buttonTitle: "Detail").padding(.top, 16)
-                VStack{
-                    FormViewWithInfo(title: "Tanggal Jatuh Tempo", profileValue: $title1, showButton: false, showButtonInfo: true, info: "Hari pembayaran atau batas waktu pembayaran harus dilakukan oleh peminjam dana (debitur) ke pemberi pinjaman (kreditur).", buttonTitle: "").zIndex(/*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/).onTapGesture {
-                        showPickerJatuhTempo.toggle()
-                    }
-                    if(showPickerJatuhTempo){
-                        DatePicker(
-                            "",
-                            selection: $dateJatuhTempo.onChange(nameChanged),
-                            displayedComponents: [.date]
-                        )
-                        .datePickerStyle(GraphicalDatePickerStyle())
-                    }
-                    
-                    FormViewWithInfo(title: "Pengadilan Negeri", profileValue: $title2, showButton: true, showButtonInfo: true, info: "Pilihan domisili pengadilan negeri untuk upaya hukum penyelesaian perselisihan jika musyawarah tidak berhasil", buttonTitle: "Detail").zIndex(0.9)
-                    FormViewWithInfo(title: "Tanggal Tanda Tangan", profileValue: $title3, showButton: false, showButtonInfo: true, info: "Hari untuk peminjam dana (debitur) dan pemberi pinjaman (kreditur) bersama menandatangani surat perjanjian.", buttonTitle: "").zIndex(0.8)
-                        .onTapGesture {
-                            showPickerTandaTangan.toggle()
-                        }
-                    if(showPickerTandaTangan){
-                        DatePicker(
-                            "",
-                            selection: $dateTandaTangan.onChange(tandaTanganChanged),
-                            displayedComponents: [.date]
-                        )
-                        .datePickerStyle(GraphicalDatePickerStyle())
-                    }
-                }
-                
-                NavigationLink(
-                    destination: step4Agunan(),
-                    label: {
-                        ButtonNext(text: "Lanjutkan", isDataComplete: true).padding(.bottom, 16).padding(.top, 64)
-                    }).navigationBarTitle("Perjanjian Baru", displayMode: .inline)
-                
-            }.frame(width: UIScreen.main.bounds.width - 35,
-                    alignment: .leading)
-            
-        }
+		VStack(alignment: .leading) {
+			pageIndicator(progressNumber: 3, progressName: "Detail Pinjaman", progressDetail: "Berikutnya: Agunan").padding(.bottom, 15).padding(.top,25)
+			
+			ScrollView{
+				VStack(alignment: .leading){
+					
+					FormView(title: "Tujuan Peminjaman", profileValue: $tujuanPeminjaman, keyboardNum: false)
+					SliderViewWithForm(text1: "Pinjaman Maksimal", text2: "Rp 50000000", title: "Jumlah Pinjaman", type: 0)
+					SliderViewWithForm(text1: "Bunga Maksimal", text2: "6 % per tahun", title: "Bunga", type: 1)
+					SliderViewWithForm(text1: "Tenor Maksimal", text2: "24 bulan", title: "Tenor", type: 2)
+					FormViewWithInfo(title: "Metode Pembayaran", profileValue: $title, showButton: true, showButtonInfo: false, info: "", buttonTitle: "Detail").padding(.top, 16)
+					VStack{
+						FormViewWithInfo(title: "Tanggal Jatuh Tempo", profileValue: $title1, showButton: false, showButtonInfo: true, info: "Hari pembayaran atau batas waktu pembayaran harus dilakukan oleh peminjam dana (debitur) ke pemberi pinjaman (kreditur).", buttonTitle: "").zIndex(/*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/).onTapGesture {
+							showPickerJatuhTempo.toggle()
+						}
+						if(showPickerJatuhTempo){
+							DatePicker(
+								"",
+								selection: $dateJatuhTempo.onChange(nameChanged),
+								displayedComponents: [.date]
+							)
+							.datePickerStyle(GraphicalDatePickerStyle())
+						}
+						
+						FormViewWithInfo(title: "Pengadilan Negeri", profileValue: $title2, showButton: true, showButtonInfo: true, info: "Pilihan domisili pengadilan negeri untuk upaya hukum penyelesaian perselisihan jika musyawarah tidak berhasil", buttonTitle: "Detail").zIndex(0.9)
+						FormViewWithInfo(title: "Tanggal Tanda Tangan", profileValue: $title3, showButton: false, showButtonInfo: true, info: "Hari untuk peminjam dana (debitur) dan pemberi pinjaman (kreditur) bersama menandatangani surat perjanjian.", buttonTitle: "").zIndex(0.8)
+							.onTapGesture {
+								showPickerTandaTangan.toggle()
+							}
+						if(showPickerTandaTangan){
+							DatePicker(
+								"",
+								selection: $dateTandaTangan.onChange(tandaTanganChanged),
+								displayedComponents: [.date]
+							)
+							.datePickerStyle(GraphicalDatePickerStyle())
+						}
+						
+					}
+					NavigationLink(
+						destination: step4Agunan(),
+						label: {
+							ButtonNext(text: "Lanjutkan", isDataComplete: true).padding(.bottom, 16).padding(.top, 64)
+						}).navigationBarTitle("Perjanjian Baru", displayMode: .inline)
+				   
+					
+	//				Spacer()
+					
+				}.padding(.top,10)
+				
+			}
+		}.frame(width: UIScreen.main.bounds.width - 35,
+				alignment: .leading)
+        
     }
     func nameChanged(to value: Date) {
         showPickerJatuhTempo.toggle()
