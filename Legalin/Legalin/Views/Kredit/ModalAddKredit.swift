07b110d6343c5b3c309488a486252079cb094675
jasonalfian <hartanto.jason@gmail.com>
2021-08-11 16:49:17 +0700

    minor edit add environment object step 3

--- Legalin/Legalin/Views/Kredit/ModalAddKredit.swift
+++ Legalin/Legalin/Views/Kredit/ModalAddKredit.swift
@@ -12,6 +12,7 @@ import SwiftUI
 struct ModalAddKredit: View {
     @Environment(\.presentationMode) var presentationMode
     @ObservedObject var kreditData = ListKreditVM()
+    @EnvironmentObject var perjanjianController: PerjanjianController
     
     var body: some View {
         NavigationView {
@@ -19,9 +20,9 @@ struct ModalAddKredit: View {
             ScrollView {
                 VStack{
                     FormView(title: "Tujuan Meminjam", profileValue: $kreditData.object.kreditTitle, keyboardNum: false).padding(.horizontal, 16).padding(.top, 16)
-                    SliderViewWithForm(text1: "Pinjaman Maksimal", text2: "Rp. 50.000.000", title: "Jumlah Pinjaman", type: 0).padding(.horizontal, 16)
-                    SliderViewWithForm(text1: "Bunga Maksimal", text2: "6 %", title: "Bunga", type: 1).padding(.horizontal, 16)
-                    SliderViewWithForm(text1: "Tenor Maksimal Maksimal", text2: "24 Bulan", title: "Tenor", type: 2).padding(.horizontal, 16)
+                    SliderViewWithForm(sliderValue: $perjanjianController.jumlahPinjaman, text1: "Pinjaman Maksimal", text2: "Rp. 50.000.000", title: "Jumlah Pinjaman", type: 0).padding(.horizontal, 16)
+                    SliderViewWithForm(sliderValue: $perjanjianController.bunga, text1: "Bunga Maksimal", text2: "6 %", title: "Bunga", type: 1).padding(.horizontal, 16)
+                    SliderViewWithForm(sliderValue: $perjanjianController.tenor, text1: "Tenor Maksimal Maksimal", text2: "24 Bulan", title: "Tenor", type: 2).padding(.horizontal, 16)
                 }
                 NavigationLink(
                     destination: UlasanKredit(),
