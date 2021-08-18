783e37c64fb151be05ca9658da1db1c4641a8372
bayuth <37474942+bayuth@users.noreply.github.com>
2021-08-18 12:49:55 +0800

    update pdfAction

--- Legalin/Legalin/Views/PerjanjianPage/PdfAction.swift
+++ Legalin/Legalin/Views/PerjanjianPage/PdfAction.swift
@@ -25,6 +25,7 @@ struct PdfAction: View {
                         
                         Text("Surat perjanjian hutang - Modal proyek katering - 25/07/2021")
                             .font(.headline)
+                            .foregroundColor(.black)
                             .multilineTextAlignment(.center)
                     }
                     .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: 228, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
