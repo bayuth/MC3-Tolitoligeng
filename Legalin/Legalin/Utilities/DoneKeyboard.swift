52668adf590c4e42de8b551772e32049d50e9a79
bayuth <37474942+bayuth@users.noreply.github.com>
2021-08-19 18:35:02 +0800

    done button on keyboard
    minus on step 3

---
+++ Legalin/Legalin/Utilities/DoneKeyboard.swift
@@ -0,0 +1,53 @@
+//
+//  TestKeyboard.swift
+//  Legalin
+//
+//  Created by Bayu Triharyanto on 18/08/21.
+//
+
+import SwiftUI
+import UIKit
+
+struct DoneKeyboard: UIViewRepresentable {
+    
+    @Binding var text: String
+    var hint: String
+    var keyType: UIKeyboardType
+    
+    func makeCoordinator() -> Coordinator {
+        return DoneKeyboard.Coordinator()
+    }
+    
+    
+    func makeUIView(context: Context) -> UITextField{
+        let textView = UITextField()
+        textView.text = hint
+        textView.font = .systemFont(ofSize: 17)
+        textView.keyboardType = keyType
+        textView.attributedPlaceholder = NSAttributedString(string: hint, attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(textView.doneButtonTapped(button:)))
+        
+        toolbar.barStyle = .default
+        toolbar.items = [spacer,doneButton]
+        toolbar.sizeToFit()
+        toolbar.setItems([spacer,doneButton], animated: true)
+        textView.inputAccessoryView = toolbar
+        return textView
+    }
+    
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        uiView.text = text
+    }
+
+}
+
+extension UITextField{
+    @objc func doneButtonTapped(button: UIBarButtonItem) -> Void{
+        self.resignFirstResponder()
+    }
+}
+
+
