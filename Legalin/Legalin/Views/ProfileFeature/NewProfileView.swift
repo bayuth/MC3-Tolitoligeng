862460667b0fc83b484de14a063545a9322efd9e
Rahmannur Rizki Syahputra <srahmannur@gmail.com>
2021-08-19 11:52:18 +0700

    Update NewProfileView.swift
    benerin spacing dkk

--- Legalin/Legalin/Views/ProfileFeature/NewProfileView.swift
+++ Legalin/Legalin/Views/ProfileFeature/NewProfileView.swift
@@ -11,6 +11,7 @@ struct NewProfileView: View {
 	
 	@ObservedObject var profiledata = profileData()
 	@State var profiles:[profileData] = []
+	@StateObject var cameraManager = CameraManager()
 	
 	@State private var showScannerSheet = false
 	@State private var texts:[ScanData] = []
@@ -42,22 +43,29 @@ struct NewProfileView: View {
 	}()
 	
 	var body: some View {
-		ScrollView {
-			ZStack {
-				VStack(alignment: .leading) {
+		VStack(alignment: .leading){
+			ScrollView{
+				ZStack {
 					VStack(alignment: .leading) {
 						
+						Text("KTP").font(.footnote).fontWeight(.medium).foregroundColor(Color(#colorLiteral(red: 0.4391747117, green: 0.4392418861, blue: 0.4391601086, alpha: 1))) .padding(.bottom,7)
+						
 						Button(action: {
-							trimKtp.showScannerSheet = true
+							if cameraManager.permissionGranted {
+								trimKtp.showScannerSheet = true
+							} else {
+								cameraManager.requestPermission()
+							}
+							
 						}, label: {
-							Text("Ambil gambar KTP untuk isi otomatis \(Image(systemName: "camera.fill"))").foregroundColor(Color(#colorLiteral(red: 0, green: 0.2837583721, blue: 0.423648268, alpha: 1)))
+							Text("Ambil gambar KTP untuk isi otomatis \(Image(systemName: "camera.fill"))").fontWeight(.regular) .foregroundColor(Color(#colorLiteral(red: 0.06274509804, green: 0.2784313725, blue: 0.4117647059, alpha: 1)))
 						})
 						Divider()
 							.fullScreenCover(isPresented: $trimKtp.showScannerSheet, content: {
 								trimKtp.makeScannerView()
 							}).padding(.bottom)
 						
-						VStack {
+						VStack(alignment: .leading) {
 							FormView(title: "NIK", profileValue: $profileController.pihak1NIK, keyboardNum: true, isDisable: $isDisable)
 							FormView(title: "Nama", profileValue: $profileController.pihak1Nama, keyboardNum: false, isDisable: $isDisable)
 							VStack(alignment: .leading){
@@ -66,7 +74,7 @@ struct NewProfileView: View {
 									.font(.body)
 									.foregroundColor(Color(#colorLiteral(red: 0.06274509804, green: 0.2784313725, blue: 0.4117647059, alpha: 1)))
 									.onTapGesture {
-									showTanggalLahir.toggle()
+										showTanggalLahir.toggle()
 									}
 								Divider()
 									.padding(.bottom)
@@ -75,6 +83,7 @@ struct NewProfileView: View {
 								DatePicker("", selection: $profileController.pihak1TanggalLahir, displayedComponents: .date)
 									.datePickerStyle(GraphicalDatePickerStyle())
 							}
+							
 							FormView(title: "Alamat", profileValue: $profileController.pihak1Alamat, keyboardNum: false, isDisable: $isDisable)
 							HStack {
 								FormView(title: "RT", profileValue: $profileController.pihak1RT, keyboardNum: true, isDisable: $isDisable)
@@ -83,28 +92,33 @@ struct NewProfileView: View {
 							FormView(title: "Kelurahan/Desa", profileValue: $profileController.pihak1Kelurahan, keyboardNum: false, isDisable: $isDisable)
 							FormView(title: "Kecamatan", profileValue: $profileController.pihak1Kecamatan, keyboardNum: false, isDisable: $isDisable)
 							FormView(title: "Kabupaten/Kota", profileValue: $profileController.pihak1Kota, keyboardNum: false, isDisable: $isDisable)
-							VStack {
+							VStack(alignment:.leading) {
 								FormView(title: "Provinsi", profileValue: $profileController.pihak1Provinsi, keyboardNum: false, isDisable: $isDisable)
 								FormView(title: "Pekerjaan", profileValue: $profileController.pihak1Pekerjaan, keyboardNum: false, isDisable: $isDisable)
 								FormView(title: "Nomor Telepon", profileValue: $profileController.pihak1NomorHP, keyboardNum: true, isDisable: $isDisable)
+								Text("Pastikan semua data yang anda masukan sudah benar dan sesuai dengan KTP anda")
+									.font(.caption2)
+									.fontWeight(.regular)
+									.foregroundColor(Color(#colorLiteral(red: 0.06274509804, green: 0.2784313725, blue: 0.4117647059, alpha: 1)))
+									.multilineTextAlignment(.leading)
+									.padding(.bottom,10)
+								Button(action: {
+									shown.toggle()
+								}, label: {
+									ButtonNext(text: "Simpan", isDataComplete: true)
+								}).padding(.bottom)
 							}
 						}
 					}
-					Button(action: {
-						shown.toggle()
-					}, label: {
-						ButtonNext(text: "Simpan", isDataComplete: true)
-					}).padding(.bottom)
-				}.padding(.horizontal)
-				
-				if shown {
-					AlertSave(shown: $shown, textField: $textfieldDisable)
+					.padding(.horizontal)
+					if shown {
+						AlertSave(shown: $shown, textField: $textfieldDisable)
+					}
 				}
+				
 			}
+			.background(shown ? Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 0.5)) : Color(#colorLiteral(red: 0.9607843137, green: 0.968627451, blue: 0.9725490196, alpha: 0)))
 		}
-		.background(shown ? Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 0.5)) : Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)))
-		
-		
 	}
 	
 	struct NewProfileView_Previews: PreviewProvider {
