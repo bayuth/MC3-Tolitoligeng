943b47a49db52a8f4392a8a0c138b56a43efecca
Achmad Fathullah <amedixro@gmail.com>
2021-08-22 20:54:07 +0700

    success get data from coredata

--- Legalin/Legalin/ViewModels/Kredit/ListKredit.swift
+++ Legalin/Legalin/ViewModels/Kredit/ListKredit.swift
@@ -9,14 +9,40 @@ import SwiftUI
 
 class ListKreditVM: ObservableObject {
     @Published var object = ItemListKredit()
+    /*
     @Published var list = [
         ItemListKredit(kreditTitle: "Nikah istri 1", tenor: 24, bunga: 6, jumlahPinjaman: 12000000, cicilanPerbulan: 2500000, offset: 0, isSwiped: false),
         ItemListKredit(kreditTitle: "Nikah istri 2", tenor: 24, bunga: 6, jumlahPinjaman: 12000000, cicilanPerbulan: 2500000, offset: 0, isSwiped: false),
         ItemListKredit(kreditTitle: "Nikah istri 3", tenor: 24, bunga: 6, jumlahPinjaman: 12000000, cicilanPerbulan: 2500000, offset: 0, isSwiped: false),
         ItemListKredit(kreditTitle: "Nikah istri 4", tenor: 24, bunga: 6, jumlahPinjaman: 12000000, cicilanPerbulan: 2500000, offset: 0, isSwiped: false),
     ]
+     */
+    
+    var coreDataVM: CoreDataViewModel = .shared
+    
+    static let shared = ListKreditVM()
+    
+    
+    @Published var list : [ItemListKredit] = []
+    
+    init() {
+        fillListDone()
+    }
     
     func hasNilField() -> Bool {
         return object.kreditTitle == "" && object.jumlahPinjaman == 0.0 && object.tenor == 0.0
+    }
+    
+    func fillListDone() {
+        
+        //Data riwayat
+        let listKredit = coreDataVM.listKredit
+        
+        list = []
+        
+        for item in listKredit{
+            list.append(ItemListKredit(kreditTitle: item.namaSimulasi ?? "", tenor: item.tenor, bunga: item.bunga, jumlahPinjaman: item.jumlahPinjaman, cicilanPerbulan: 0, offset: 0, isSwiped: false))
+        }
+        
     }
 }
