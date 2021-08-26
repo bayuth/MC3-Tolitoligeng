0a7194fcf85bdf438add8651114d3ffac5d15191
Rahmannur Rizki Syahputra <srahmannur@gmail.com>
2021-08-26 21:40:10 +0700

    benerin website page

--- Legalin/Legalin/Views/Perjanjian/Reusable/MultiLineFormView.swift
+++ Legalin/Legalin/Views/Perjanjian/Reusable/MultiLineFormView.swift
@@ -37,6 +37,7 @@ struct MultiLineFormView: View {
                 }.background(Color("emptyAlertColor").opacity(getRedIndicator() ? 0.15 : 0.0))
 			} else {
                 VStack{
+					Text(alamat)
 				AutoSizingTF(hint: "Alamat Sesuai KTP", text: $alamat, containerHeight: $containerHeight, onEnd: {
 					UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
 				}, isDisable: isDisable)
@@ -84,32 +85,25 @@ struct AutoSizingTF: UIViewRepresentable {
 	func makeUIView(context: Context) -> UITextView {
 //		let textView = UITextField()
 		let textView = UITextView()
-		if self.text == "" && !isDisable{
+		if self.text == ""{
 			textView.text = hint
-			textView.textColor = UIColor(Color("textColor"))
+//			textView.textColor = UIColor(Color("textColor"))
 			textView.font = .systemFont(ofSize: 17)
-			
+
 		} else {
 			textView.text = text
 			textView.font = .systemFont(ofSize: 17)
 //			textView.textColor = UIColor(Color(.black))
-			
+
 		}
+		textView.text = text
+		textView.font = .systemFont(ofSize: 17)
+//		textView.textColor = UIColor(Color(.black))
 		if isDisable {
 			textView.textColor = UIColor(Color("labelColor"))
 		} else {
 			textView.textColor = UIColor(Color("textColor"))
 		}
-//		display hint
-//		if textView.text == nil {
-//			textView.text = hint
-////			textView.textColor = .gray
-//			textView.font = .systemFont(ofSize: 18)
-//		} else {
-//			textView.text = text
-////			textView.textColor = .black
-//			textView.font = .systemFont(ofSize: 18)
-//		}
 		
 //		textView.text = text
 //		textView.font = .systemFont(ofSize: 18)
@@ -178,5 +172,6 @@ struct AutoSizingTF: UIViewRepresentable {
 			}
 		}
 	}
+	
 }
 
