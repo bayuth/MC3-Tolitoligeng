90d0a0dfe7031b7ef1b836ec7a9aa535419a2ee2
jasonalfian <hartanto.jason@gmail.com>
2021-08-31 10:59:58 +0700

    Replace keyboard step4 with done keyboard. Fix Homepage Kredit Mas Achmad

--- Legalin/Legalin/Views/Kredit/KreditView.swift
+++ Legalin/Legalin/Views/Kredit/KreditView.swift
@@ -14,6 +14,7 @@ struct KreditView: View {
     
     @Environment(\.presentationMode) var presentationMode
     @State private var isPresented = false
+    @State var actionState: Int? = 0
     
     var body: some View {
         NavigationView{
@@ -23,24 +24,23 @@ struct KreditView: View {
                 }else {
                     List{
                         ForEach(coreData.listKredit, id:\.uuid){ item in
+                            
                             ZStack{
                                 NavigationLink(
-                                    destination: DetailKredit(kredit: item, index: getIndex(item: item), onDelete: {
-                                        kreditData.deleteKredit(index: getIndex(item: item))
-                                    }),
+                                    destination: DetailKredit(kredit: item, onDelete: {
+                                        kreditData.fillListDone()
+                                    }, actionState: $actionState),
+                                    tag: 4, selection: $actionState,
                                     label: {
-                                        EmptyView()
+                                        KreditCardView(item: item)
                                     }
                                 )
                                 .opacity(0)
-                                KreditCardView(item:
-                                item
-                                )
+                                KreditCardView(item:item)
+                                    .simultaneousGesture(TapGesture().onEnded{
+                                        actionState = 4
+                                    })
                             }
-                                .foregroundColor(.black)
-                                .simultaneousGesture(TapGesture().onEnded{
-                                    kreditData.object = kreditData.list[getIndex(item: item)]
-                                })
                         }.onDelete(perform: deleteKredit)
                     }.listStyle(PlainListStyle())
                 }
--- Legalin/Legalin/Views/Perjanjian/Reusable/inputTextViewCell.swift
+++ Legalin/Legalin/Views/Perjanjian/Reusable/inputTextViewCell.swift
@@ -18,24 +18,44 @@ struct inputTextViewCell: View {
     
     var body: some View {
         
-        VStack(alignment: .leading, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
-            Text(title).font(.footnote).fontWeight(.light).padding(.horizontal)
-                .padding(.top)
+//        VStack(alignment: .leading, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+//            Text(title).font(.footnote).fontWeight(.light).padding(.horizontal)
+//                .padding(.top)
+//            
+//            VStack{
+//            HStack{
+//            
+//                TextField(emptyStateString, text: $textViewValue, onEditingChanged: { _ in perjanjianController.setNextButtonState() })
+//                    
+////            TextField(emptyStateString, text: $textViewValue)
+//                
+//                .keyboardType(keyboardNum ? .numberPad : .default)
+//            }.padding(.horizontal)
+//            
+//            Divider()
+//            }.background(Color("emptyAlertColor").opacity(getRedIndicator() ? 0.15 : 0.0))
+//            
+//        })
+        
+        VStack(alignment: .leading) {
             
+            Text(title).font(.footnote).fontWeight(.regular).foregroundColor(Color("labelColor")).padding(.horizontal)
+            //                TextField(title, text: $profileValue).font(.body).foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))).padding(.horizontal)
             VStack{
-            HStack{
-            
-                TextField(emptyStateString, text: $textViewValue, onEditingChanged: { _ in perjanjianController.setNextButtonState() })
-                    
-//            TextField(emptyStateString, text: $textViewValue)
+                DoneKeyboard(text: $textViewValue, hint: emptyStateString, keyType: keyboardNum ? UIKeyboardType.numberPad : UIKeyboardType.default, onEndEditing: {perjanjianController.setNextButtonState()},clearOnStartEdit: false)
+                    .foregroundColor(Color("formViewColor"))
+                    .lineLimit(3)
+                    .accentColor(Color("tabBarColor"))
+                    .font(.body)
+//                    .disabled(isDisable)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: 20)
+                    .padding(.horizontal)
+                Divider()
                 
-                .keyboardType(keyboardNum ? .numberPad : .default)
-            }.padding(.horizontal)
-            
-            Divider()
             }.background(Color("emptyAlertColor").opacity(getRedIndicator() ? 0.15 : 0.0))
             
-        })
+        }
+        .padding(.bottom)
         
     }
     func getRedIndicator() -> Bool{
